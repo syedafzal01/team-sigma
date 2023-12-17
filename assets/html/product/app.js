968a15508e420ea864fc4a779787f54c1e9b84e8
Mohammed Muzammil Shareef <mohdmuzzammil633@gmail.com>
2023-12-17 05:56:44 +0530

    Add files via upload

--- assets/html/product/app.js
+++ assets/html/product/app.js
@@ -84,9 +84,9 @@ const addCartToHTML = () => {
                 </div>
                 <div class="totalPrice">$${info.price * item.quantity}</div>
                 <div class="quantity">
-                    <span class="minus"><</span>
+                    <span class="minus">-</span>
                     <span>${item.quantity}</span>
-                    <span class="plus">></span>
+                    <span class="plus">+</span>
                 </div>
             `;
         })
